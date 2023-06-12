2403283834525eb34753a875348338a693afa90b
Javier Sanchez <sanchezfdez.javier@gmail.com>
2023-06-12 19:45:15 +0200

    Implements pretty much all

--- README.md
+++ README.md
@@ -1,21 +1,23 @@
-# Jupyter Backend API
+![API Preview](api-preview.png)
 
 This is the API for a lightweight version of Jupyter notebooks built using FastAPI and MongoDB. It allows users to create, read, update, and delete Python notebooks and execute Python code within these notebooks. It's designed to be simple and intuitive, making it easy for anyone to run Python code in an interactive manner ✨
 
 
 ## Contributing
 
-You can use the environments created by `tox` for development:
+You can use the environments created by `tox` for development. From the root of the repository run
 
 ```shell
 tox --notest -e unit
 source .tox/unit/bin/activate
 ```
 
-### Create a `.env` file with the database connection string under the `src` directory
+### Setup the database endpoint
 
-```env
-MONGO_CONNECTION_STRING=<your db connection string>
+Create a `.env` file with the [database connection string](https://www.mongodb.com/docs/guides/atlas/connection-string/) under the `src` directory
+
+```bash
+echo "MONGO_CONNECTION_STRING=<your database connection string>" > src/.env
 ```
 
 ### Start the development server
@@ -26,20 +28,20 @@ Go into the `src` directory
 cd src
 ```
 
+Start the development server
+
 ```bash
 uvicorn main:app --reload
 ```
 
 ### Swagger UI
 
-You can acccess the Swagger UI by going to http://localhost:8000/docs
-
-![API Preview](api-preview.png)
+You can access the Swagger UI by going to http://localhost:8000/docs
 
 ### Testing
 
 This project uses `tox` for managing test environments. There are some pre-configured environments
-that can be used for linting and formatting code when you're preparing contributions to the charm:
+that can be used for linting and formatting code when you're preparing contributing.
 
 ```shell
 tox -e lint          # code style
--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
-"""Main module my cool API 😉."""
+"""Main module for my cool Jupyter Notebooks API 😉."""
 
 import os
+from datetime import datetime
+from typing import Any, Dict, List, Optional
 
 from bson.objectid import ObjectId
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from IPython.core.interactiveshell import InteractiveShell
+from pydantic import BaseModel
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
@@ -32,6 +37,55 @@ app = FastAPI(
     version="0.0.1",
 )
 
+# FIXME: Not super secure 😆
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+
+class ExecutionContextManager:
+    """Manage execution contexts for notebooks."""
+
+    def __init__(self):
+        """Create a new execution context manager."""
+        self.contexts = {}
+
+    def create_context(self, notebook_id: str) -> InteractiveShell:
+        """Create a new execution context for a notebook.
+
+        Args:
+            notebook_id (str): The id of the notebook to create a context for.
+
+        Returns:
+            InteractiveShell: The execution context for the notebook.
+        """
+        shell = InteractiveShell()
+        self.contexts[notebook_id] = shell
+        return shell
+
+    def get_context(self, notebook_id: str) -> Optional[InteractiveShell]:
+        """Get the execution context for a notebook.
+
+        Args:
+            notebook_id (str): The id of the notebook to get the context for.
+
+        Returns:
+            InteractiveShell: The execution context for the notebook.
+        """
+        return self.contexts.get(notebook_id)
+
+    def remove_context(self, notebook_id: str) -> None:
+        """Remove the execution context for a notebook.
+
+        Args:
+            notebook_id (str): The id of the notebook to remove the context for.
+        """
+        self.contexts.pop(notebook_id, None)
+
 
 def document_to_dict(document) -> dict:
     """Convert a MongoDB document to a Python dict, converting '_id' to string."""
@@ -41,13 +95,21 @@ def document_to_dict(document) -> dict:
 
 @app.get("/")
 def read_root() -> dict:
-    """Return a simple message."""
+    """Return a simple message.
+
+    Returns:
+        dict: A simple message.
+    """
     return {"👋 Hello": "Jupyter clone!"}
 
 
 @app.get("/ping-db")
 def ping() -> dict:
-    """Ping the database."""
+    """Ping the database.
+
+    Returns:
+        dict: A simple message.
+    """
     try:
         response: dict = client.admin.command("ping")
         return {"Ping Database": f"🟢 Success, database is available {response}"}
@@ -57,7 +119,11 @@ def ping() -> dict:
 
 @app.get("/notebooks")
 def get_notebooks() -> dict:
-    """Get all documents in the collection."""
+    """Get all documents in the collection.
+
+    Returns:
+        dict: A list of all documents in the collection.
+    """
     try:
         cursor = collection.find()
         documents = [document_to_dict(document) for document in cursor]
@@ -72,11 +138,14 @@ def get_user_notebooks(user_email: str) -> dict:
 
     Args:
         user_email (str): The email of the user whose documents to retrieve.
+
+    Returns:
+        dict: A list of all documents belonging to the user.
     """
     try:
         cursor = collection.find({"user": user_email})
-        document_names = [doc["filename"] for doc in cursor]
-        return {"document_names": document_names}
+        user_documents = [{"filename": doc["filename"], "_id": str(doc["_id"])} for doc in cursor]
+        return {"user_documents": user_documents}
     except Exception as e:
         return {"error": str(e)}
 
@@ -87,6 +156,9 @@ def get_notebook(document_id: str) -> dict:
 
     Args:
         document_id (str): The id of the document to retrieve.
+
+    Returns:
+        dict: The document with the given id.
     """
     try:
         oid: ObjectId = ObjectId(document_id)
@@ -104,6 +176,9 @@ def delete_notebook(document_id: str) -> dict:
 
     Args:
         document_id (str): The id of the document to delete.
+
+    Returns:
+        dict: A message indicating whether the document was deleted successfully.
     """
     try:
         oid: ObjectId = ObjectId(document_id)
@@ -114,3 +189,149 @@ def delete_notebook(document_id: str) -> dict:
             return {"error": "No document found with this id to delete."}
     except Exception as e:
         return {"error": str(e)}
+
+
+class Document(BaseModel):
+    """Create notebook POST request body model."""
+
+    filename: str
+    user: str
+
+
+@app.post("/notebook")
+def create_notebook(document: Document) -> dict:
+    """Create a new document.
+
+    Args:
+        document (Document): The document to create.
+
+    Returns:
+        dict: A message indicating whether the document was created successfully.
+    """
+    filename = document.filename
+    user = document.user
+
+    default_notebook_structure = {
+        "cells": [],
+        "metadata": {
+            "kernelspec": {
+                "display_name": "Python 3 (ipykernel)",
+                "language": "python",
+                "name": "python3",
+            },
+            "language_info": {
+                "codemirror_mode": {"name": "ipython", "version": 3},
+                "file_extension": ".py",
+                "mimetype": "text/x-python",
+                "name": "python",
+                "nbconvert_exporter": "python",
+                "pygments_lexer": "ipython3",
+                "version": "3.8.10",
+            },
+        },
+        "nbformat": 4,
+        "nbformat_minor": 5,
+    }
+
+    try:
+        new_document = {
+            "notebook": default_notebook_structure,
+            "user": user,
+            "filename": filename,
+            "created_at": datetime.now(),
+            "updated_at": datetime.now(),
+        }
+        result = collection.insert_one(new_document)
+        return {"success": f"Document with id {result.inserted_id} created successfully."}
+    except Exception as e:
+        return {"error": str(e)}
+
+
+class Cell(BaseModel):
+    """Create cell model."""
+
+    cell_type: str
+    source: List[str]
+    metadata: Optional[Dict[str, Any]] = {}
+    execution_count: Optional[int] = None
+    outputs: Optional[List[Dict[str, Any]]] = []
+
+
+@app.delete("/notebook/{notebook_id}/cell/{cell_id}")
+def delete_cell(notebook_id, cell_id) -> dict:
+    """Delete a specific cell by its id from a notebook.
+
+    Args:
+        notebook_id (str): The id of the notebook containing the cell.
+        cell_id (int): The id of the cell to delete.
+
+    Returns:
+        dict: A message indicating whether the cell was deleted successfully.
+    """
+    try:
+        oid: ObjectId = ObjectId(notebook_id)
+        result = collection.update_one(
+            {"_id": oid},
+            {"$pull": {"notebook.cells": {"id": cell_id}}, "$set": {"updated_at": datetime.now()}},  # noqa E501
+        )
+        if result.modified_count:
+            return {"success": f"Cell with id {cell_id} deleted successfully from notebook {notebook_id}."}  # noqa E501
+        else:
+            return {"error": "No cell found with this id to delete."}
+    except Exception as e:
+        return {"error": str(e)}
+
+
+context_manager = ExecutionContextManager()
+
+
+class ExecuteCodeBody(BaseModel):
+    """Execute code request body model."""
+
+    source: str
+
+
+@app.post("/notebook/{notebook_id}/cell/{cell_id}/execute_and_update")
+def execute_and_update_code(notebook_id: str, cell_id: str, body: dict) -> dict:
+    """Execute code and update cell.
+
+    Args:
+        notebook_id (str): The id of the notebook containing the cell.
+        cell_id (str): The id of the cell to add/update.
+        body (dict): The request body containing the code to execute.
+
+    Returns:
+        dict: The output of the code.
+    """
+    code = body.get("source", "")
+    try:
+        shell = context_manager.get_context(notebook_id) or context_manager.create_context(notebook_id)  # noqa E501
+        result = shell.run_cell(code)
+        cell = {"id": cell_id, "source": code, "outputs": str(result)}
+
+        document = collection.find_one({"_id": ObjectId(notebook_id), "notebook.cells.id": cell_id})  # noqa E501
+        if document is None:
+            # Cell doesn't exist, push new cell
+            update_result = collection.update_one(
+                {"_id": ObjectId(notebook_id)}, {"$push": {"notebook.cells": cell}}
+            )  # noqa E501
+            if update_result.matched_count < 1:
+                return {"output": None, "error": "Failed to insert cell"}
+        else:
+            # Cell exists, update it
+            update_result = collection.update_one(
+                {"_id": ObjectId(notebook_id), "notebook.cells.id": cell_id},
+                {
+                    "$set": {
+                        "notebook.cells.$.source": code,
+                        "notebook.cells.$.outputs": str(result),
+                        "updated_at": datetime.now(),
+                    }
+                },
+            )
+            if update_result.matched_count < 1:
+                return {"output": None, "error": "Failed to update cell"}
+
+        return {"output": str(result), "error": None}
+    except Exception as e:
+        return {"output": None, "error": str(e)}
